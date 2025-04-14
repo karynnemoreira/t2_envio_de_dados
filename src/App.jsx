911a0bb372cc54089fa01b6a24b2da247864e97a
karynnemoreira <karynnemoreira@yahoo.com.br>
2025-04-14 19:50:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+//import Contador from "./components/Contador.jsx"
+import Solicitacao from "./components/Solicitacao.jsx"
+
+function App(){
+  return(
+    <>
+   {/* <Contador /> */}
+   <Solicitacao />
+   </>
+  )
+}
+
+export default App
